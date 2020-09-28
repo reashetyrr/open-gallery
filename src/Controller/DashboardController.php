9747a9b3a72754f0bc7e0fd34a71fa566cbc2e98
reashetyrr <6582364+reashetyrr@users.noreply.github.com>
2020-09-28 12:05:19 +0200

    add some bases

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DashboardController
+ * @package App\Controller
+ * @Route("/a")
+ */
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function index()
+    {
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
