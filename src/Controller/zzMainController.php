6e3739df6a98b829726b9e9338ff1b99492b93e7
reashetyrr <6582364+reashetyrr@users.noreply.github.com>
2020-09-30 13:48:21 +0200

    setup check for required account

--- src/Controller/zzMainController.php
+++ src/Controller/zzMainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Setting;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,6 +16,9 @@ class zzMainController extends BaseController
         // this is a check to see if the database has been connected (aka setup) and if the admin user has been created
         if (!$this->is_connected()) return $this->redirectToRoute('setup');
 
+        $auth_settings = $this->getDoctrine()->getRepository(Setting::class)->findOneBy(['setting_type' => 'account_required']);
+
+        if ($auth_settings->getSettingValue() === '1' && !$this->getUser()) return $this->redirectToRoute('app_login');
 
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
